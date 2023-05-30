d74f8df87d336161ffffb48dcb7e2142c170eec6
Andrea Creazzi <andreacreazzi@libero.it>
2023-05-30 18:35:20 +0200

    add css and js (complete)

--- js/script.js
+++ js/script.js
@@ -5,17 +5,28 @@ console.log(`JS OK`);
 const gameElement = document.getElementById(`game`)
 
 let numbers = `100`
-let testo = ``
+let testo = ` `
+
+gameElement.classList.add(`btn`)
+
+gameElement.classList.add(`btn-lg`)
+
+let background;
 
 // Dati concreti
 for(let i = 1 ; i <= numbers ; i++){
     testo = i;
+    background = `blue`
     if(i % 15 == 0){
         testo =`fizzbuzz`;
+        background = `red`
     }else if(i % 3 == 0){
         testo = `fizz`;
+        background = `green`
     }else if(i % 5 == 0){
         testo = `buzz`;
+        background = `yellow`
     }
     console.log(testo);
+    gameElement.innerHTML += `<div class="card ${background} display p-3 m-3">${testo}<div>`;
 };
