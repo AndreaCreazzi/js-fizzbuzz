18d0ea9a55a46d8dce3b6a31a650cb56a00359f0
Andrea Creazzi <andreacreazzi@libero.it>
2023-05-30 14:32:13 +0200

    add js ( 1 to 100 ) and html container

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 // Controllo JS
-console.log(`JS OK`)
+console.log(`JS OK`);
+
+// Dati preliminari
+const gameElement = document.getElementById(`game`)
+
+let numbers = `100`
+let text = ``
+
+// Dati concreti
+for(let i = 1 ; i <= numbers ; i++){
+    console.log(i)
+};
