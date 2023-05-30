13a6303def25d77c7d382dd64fb37996d6822d05
Andrea Creazzi <andreacreazzi@libero.it>
2023-05-30 16:36:57 +0200

    add js fizzbuzz (complete in console)

--- js/script.js
+++ js/script.js
@@ -5,9 +5,17 @@ console.log(`JS OK`);
 const gameElement = document.getElementById(`game`)
 
 let numbers = `100`
-let text = ``
+let testo = ``
 
 // Dati concreti
 for(let i = 1 ; i <= numbers ; i++){
-    console.log(i)
+    testo = i;
+    if(i % 15 == 0){
+        testo =`fizzbuzz`;
+    }else if(i % 3 == 0){
+        testo = `fizz`;
+    }else if(i % 5 == 0){
+        testo = `buzz`;
+    }
+    console.log(testo);
 };
